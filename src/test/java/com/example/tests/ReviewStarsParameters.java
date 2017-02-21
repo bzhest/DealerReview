39d0de1201713077f43e2ff0cba9d30701a68ea6
bzhest <bzhest@gmail.com>
2017-02-21 21:13:12 +0200

    add review checkboxes loop

--- src/test/java/com/example/tests/ReviewStarsParameters.java
+++ src/test/java/com/example/tests/ReviewStarsParameters.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.*;
 
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -17,17 +18,30 @@ public class ReviewStarsParameters extends TestBase {
     }
 
 
-    @Test(enabled = false) //этот тест будет выполняться первым
+    @Test//(enabled = false) //этот тест будет пропущен
     public void addOneStarParameter() {
         app.getFormHelper().loginToDMS("andrey.bzhestovskyy@xloo.com", "#login", "andrey87", "#password");
         WebElement toolsMenu = app.getWebDriverHelper().getDriver().findElements(By.cssSelector(".v9-main-item.v9_sub")).get(4);
+        // захожу на Landing page в Settings > Review
         toolsMenu.click();
         WebElement ReviewImage = app.getWebDriverHelper().getDriver().findElement(By.cssSelector(".big-module-item[href*='review-settings']"));
+        //кликаю на иконку Review
         ReviewImage.click();
-        if(app.getWebDriverHelper().getDriver().findElement(By.cssSelector("#addon_make_a_page_2")).getAttribute("checked") == null){
-            app.getLoaderHelper().waitForJSandJQueryToLoad();
-            app.getWebDriverHelper().getDriver().findElement(By.cssSelector("#addon_make_a_page_2")).click();
+        //Получаю список чекбоксов на странице
+        List<WebElement> checkboxes = app.getWebDriverHelper().getDriver().findElements(By.cssSelector("input[name*=_visible]"));
+        //Прохожу по всем чекбоксам, если чекбокс отмечен - снимаем выделение
+        for (int x=0; x<5; x++) {
+            if (checkboxes.get(x).isSelected()) {
+                checkboxes.get(x).click();
+                app.getLoaderHelper().waitForJSandJQueryToLoad();
+            }
         }
+        app.getLoaderHelper().waitForJSandJQueryToLoad();
+        WebElement buttonSave = app.getWebDriverHelper().getDriver().findElements(By.cssSelector(".sp")).get(0);
+        //Сохраняю настройки
+        buttonSave.click();
+
+
     }
 }
 
