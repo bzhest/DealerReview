8e74d55cbec8ac67a64530b0f80f9ab79a6d0a3a
bzhest <bzhest@gmail.com>
2017-02-28 17:44:26 +0200

    c

--- src/test/java/com/example/tests/ReviewStarsParameters.java
+++ src/test/java/com/example/tests/ReviewStarsParameters.java
@@ -18,11 +18,11 @@ public class ReviewStarsParameters extends TestBase {
 
     @BeforeClass(alwaysRun = true)
     public void loginToDMS() throws Exception {
-        app.getWebDriverHelper().getDriver().get("http://www.solomia.andreyb.ixloo.com/" + "dms/login");
+        app.getWebDriverHelper().getDriver().get("http://www.solomia.andreyb.ixloo.com/" + "dms");
     }
 
     //Проверка, если параметры со звездочками не выставлены, их нет и в самой форме
-    @Test//(enabled = false) //этот тест будет пропущен
+    @Test(enabled = false) //этот тест будет пропущен
     public void addNoneStarParameters() {
         app.getFormHelper().loginToDMS("andrey.bzhestovskyy@xloo.com", "#login", "andrey87", "#password");
         WebElement toolsMenu = app.getWebDriverHelper().getDriver().findElements(By.cssSelector(".v9-main-item.v9_sub")).get(4);
@@ -52,16 +52,13 @@ public class ReviewStarsParameters extends TestBase {
         Assert.assertEquals(starNumberParameter.size(),0);
     }
 
+/**
+ * Проверяем, что выставленный параметр Your Overall Rating в DMS отображается в DWS */
 
         @Test
         public void addYourOverallRatingParameter() {
 
-            app.getNavigationHelper().closeSecondTabAndGoToFirstTab();
-            app.getWebDriverHelper().getDriver().get(app.getWebDriverHelper().baseUrl + "dms");
-            String url = app.getWebDriverHelper().getDriver().getCurrentUrl();
-            //Если при переходе в DMS оказывается, что я не залогинен - я логинюсь
-            if(url == app.getWebDriverHelper().baseUrl + "dms/login")
-            {
+            if(app.getWebDriverHelper().getDriver().getCurrentUrl() != app.getWebDriverHelper().baseUrl + "dms"){
                 app.getFormHelper().loginToDMS("andrey.bzhestovskyy@xloo.com", "#login", "andrey87", "#password");
             }
             WebElement toolsMenu = app.getWebDriverHelper().getDriver().findElements(By.cssSelector(".v9-main-item.v9_sub")).get(4);
@@ -70,20 +67,27 @@ public class ReviewStarsParameters extends TestBase {
             WebElement ReviewImage = app.getWebDriverHelper().getDriver().findElement(By.cssSelector(".big-module-item[href*='review-settings']"));
             //кликаю на иконку Review
             ReviewImage.click();
-            WebElement YourOverallRatingParameter = app.getWebDriverHelper().getDriver().findElement(By.cssSelector("input[name*=0_visible]"));
+            WebElement YourOverallRatingParameter = app.getWebDriverHelper().getDriver().findElements(By.cssSelector("input[name*=_visible]")).get(0);
             List<WebElement> checkboxes = app.getWebDriverHelper().getDriver().findElements(By.cssSelector("input[name*=_visible]"));
             //Прохожу по всем чекбоксам, если чекбокс отмечен - снимаем выделение
-            if ( !YourOverallRatingParameter.isSelected() )
-            {
-                YourOverallRatingParameter.click();
-            }
+
             for (int x=0; x<5; x++) {
-                if (checkboxes.get(x).isSelected() && checkboxes.get(x)!=YourOverallRatingParameter) {
+                if (checkboxes.get(x).isSelected()) {
                     checkboxes.get(x).click();
                     app.getLoaderHelper().waitForJSandJQueryToLoad();
                 }
-
             }
+            //после того, как все чекбоксы сняты, выделяю чекбокс YourOverallRatingParameter
+            YourOverallRatingParameter.click();
+            WebElement buttonSave = app.getWebDriverHelper().getDriver().findElements(By.cssSelector(".sp")).get(0);
+            //Сохраняю настройки
+            buttonSave.click();app.getNavigationHelper().openHomePage();
+            app.getNavigationHelper().fromHomePage_toDealerReviewForm();
+            //app.getLoaderHelper().waitForJSandJQueryToLoad();
+            WebElement textForYourOverallRatingParameter = app.getWebDriverHelper().getDriver().findElement(By.cssSelector(".starLabel"));
+            String text = textForYourOverallRatingParameter.getText().trim();
+            Assert.assertEquals("Your Overall Rating", text);
+
         }
 
 }
@@ -98,3 +102,5 @@ public class ReviewStarsParameters extends TestBase {
 
 
 //$("input[name*=4_visible]").attr('checked') если чекбокс отмечен, ставит true
+
+
